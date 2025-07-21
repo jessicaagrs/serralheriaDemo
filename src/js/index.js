b2f3608b0793614a1fea5e458c6da00b280cf3ac
Jessica Aguiar <jessicaag.rs@gmail.com>
2025-07-21 09:13:38 -0300

    feat: enhance footer and add animations
    ✔️ Adds footer section with contact information and budget request button
    ✔️ Implements scroll-to-top button functionality
    ✔️ Introduces fade-in and slide-in animations for elements
    ✔️ Updates styles for footer and animations
    ✔️ Refactors global styles to include smooth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -35,3 +35,83 @@ if (carrousel) {
     e.preventDefault();
   });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonBackToTop = document.querySelector('#arrow-up');
+  const header = document.querySelector('#home');
+  const textHome = document.querySelector('.home__text');
+  const imageHome = document.querySelector('.home__image');
+  const testimonials = document.querySelector('.testimonials__cards');
+  const btnBudget = document.querySelector('#budget-button');
+
+  if (buttonBackToTop && header) {
+    observerElement(header, (isView) => {
+      hideElement(isView, buttonBackToTop);
+    });
+  }
+
+  if (textHome && imageHome) {
+    observerElement(textHome, (isView) => {
+      fadeInLeft(isView, textHome);
+    });
+
+    observerElement(imageHome, (isView) => {
+      fadeInRight(isView, imageHome);
+    });
+  }
+
+  if (testimonials) {
+    observerElement(testimonials, (isView) => {
+      slideInFwdCenter(isView, testimonials);
+    });
+  }
+
+  if (btnBudget) {
+    observerElement(btnBudget, (isView) => {
+      vibrationEffect(isView, btnBudget);
+    });
+  }
+});
+
+function hideElement(isView, element) {
+  if (isView) {
+    element.style.display = 'none';
+  } else {
+    element.style.display = 'block';
+  }
+}
+
+function fadeInLeft(isView, element) {
+  element.classList.toggle('fade-in-left', isView);
+}
+
+function fadeInRight(isView, element) {
+  element.classList.toggle('fade-in-right', isView);
+}
+
+function slideInFwdCenter(isView, element) {
+  element.classList.toggle('slide-in-fwd-center', isView);
+}
+
+function vibrationEffect(isView, element) {
+  element.classList.toggle('vibrate-1', isView);
+}
+
+function observerElement(element, callback) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          callback(true, element);
+        } else {
+          callback(false, element);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  observer.observe(element);
+}
